2e0f7671599c9683a2783f243be7856a36c52dcd
Gael <demondragong@gmail.com>
2022-12-15 17:33:28 +0100

    adjust config for GA and PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,26 +3,27 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://xn--gal-kma.com/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
+          siteName: "Gaël de Mondragon", // Used in manifest.json
+          shortName: "Gaël", // Used in manifest.json
+          description: "Gaël de Mondragon - Freelance web developer",
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
-          display: "minimal-ui", // Used in manifest.json
+          display: "standalone", // Used in manifest.json
         },
         contentDirectory: "./content",
         blogSettings: {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
+        googleAnalytics: {
+            trackingId: "G-ERDQ3FBKZZ",
+            anonymize: true, // Default true
+            environments: [] // Default ["production"]
+        }
       },
     },
   ],
